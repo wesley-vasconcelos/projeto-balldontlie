e9c40d9757f2cc69cfd7ca475106f7ee10885e1d
Wesley de Vasconcelos Silva <wesley.vasconcelos@ish.com.br>
2023-12-20 12:23:25 -0300

    adjust files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.html", "./src/**/*.vue"],
-  theme: {
-    extend: {},
+import { fontFamily as _fontFamily } from "tailwindcss/defaultTheme";
+export const content = ["../index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"];
+export const darkMode = "class";
+export const theme = {
+  extend: {
+    screens: {
+      short: { max: "1280px" },
+      normal: { min: "1281px" },
+    },
+    fontFamily: {
+      sans: ["Inter", ..._fontFamily.sans],
+    },
   },
-  plugins: [],
 };
+export const variants = {
+  extend: {},
+};
+export const plugins = [
+  require("@tailwindcss/forms"),
+  require("tailwind-scrollbar"),
+];
